d98d5c8e8c171046ba0f669123026d7a156caa5a
Carrie <carrie.bilenker@gmail.com>
2017-06-15 20:31:27 -0700

    adds google chart basic column graph

--- first-chart.js
+++ first-chart.js
@@ -7,27 +7,41 @@ google.charts.setOnLoadCallback(drawBasic);
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
-function drawChart() {
+function drawBasic() {
 
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'People who like it');
-  data.addRows([
-    ['Tomato', 6],
-    ['Pepperoni', 7],
-    ['Shrimp', 1],
-    ['Pineapple', 6],
-    ['Peppers', 4]
-  ]);
+      var data = new google.visualization.DataTable();
+      data.addColumn('timeofday', 'Time of Day');
+      data.addColumn('number', 'Motivation Level');
 
-  // Set chart options
-  var options = {'title':'Pizza Topping Popularity',
-                 'width':400,
-                 'height':300,
-                 'pieHole': 0.4};
+      data.addRows([
+        [{v: [8, 0, 0], f: '8 am'}, 1],
+        [{v: [9, 0, 0], f: '9 am'}, 2],
+        [{v: [10, 0, 0], f:'10 am'}, 3],
+        [{v: [11, 0, 0], f: '11 am'}, 4],
+        [{v: [12, 0, 0], f: '12 pm'}, 5],
+        [{v: [13, 0, 0], f: '1 pm'}, 6],
+        [{v: [14, 0, 0], f: '2 pm'}, 7],
+        [{v: [15, 0, 0], f: '3 pm'}, 8],
+        [{v: [16, 0, 0], f: '4 pm'}, 9],
+        [{v: [17, 0, 0], f: '5 pm'}, 10],
+      ]);
 
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
-}
+      var options = {
+        title: 'Motivation Level Throughout the Day',
+        hAxis: {
+          title: 'Time of Day',
+          format: 'h:mm a',
+          viewWindow: {
+            min: [7, 30, 0],
+            max: [17, 30, 0]
+          }
+        },
+        vAxis: {
+          title: 'Rating (scale of 1-10)'
+        }
+      };
+      var chart = new google.visualization.ColumnChart(
+        document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+  }
